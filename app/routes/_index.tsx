94f0396324e3b64db17d2a3da7e4b99e1bb43b68
yumotoyoshiaki <yoshiosmile68@gmail.com>
2024-05-11 16:45:11 +0900

    init

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,13 +4,9 @@
 import { json } from "@remix-run/cloudflare";
 import type {
   MetaFunction,
-  LoaderFunctionArgs,
-  ActionFunctionArgs,
 } from "@remix-run/cloudflare";
-import { Form, useActionData, useLoaderData } from "@remix-run/react";
-import { eq } from "drizzle-orm";
-import { drizzle } from "drizzle-orm/d1";
-import { users } from "~/drizzle/schema.server";
+import { Form } from "@remix-run/react";
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -22,72 +18,10 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export async function action({ request, context }: ActionFunctionArgs) {
-  const formData = await request.formData();
-  const action = formData.get("_action") as string;
-  const cloudflare = context.cloudflare as any;
-
-  if (action === "delete") {
-    const id = Number(formData.get("id")); // Convert id to number
-    const db = drizzle(cloudflare.env.DB);
-    await db.delete(users).where(eq(users.id, id)).execute();
-    return json({ message: "Resource deleted" }, { status: 200 });
-  } else if (action === "add") {
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const db = drizzle(cloudflare.env.DB);
-    const exist = await db
-      .select()
-      .from(users)
-      .where(eq(users.email, email))
-      .execute();
-
-    if (exist.length > 0) {
-      return json({ message: "Resource already exists" }, { status: 400 });
-    }
-
-    await db.insert(users).values({ email, password }).execute();
-
-    return json({ message: "Resource added" }, { status: 201 });
-  }
-}
-export async function loader({ context }: LoaderFunctionArgs) {
-  const cloudflare = context.cloudflare as any;
-  const db = drizzle(cloudflare.env.DB);
-
-  const resourceList = await db
-    .select({
-      id: users.id,
-      email: users.email,
-      password: users.password,
-    })
-    .from(users)
-    .orderBy(users.id);
-
-  return json({
-    resourceList,
-  });
-}
 
 export default function Index() {
-  const { resourceList } = useLoaderData<typeof loader>();
-  const d = useActionData<typeof action>();
-
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      {d?.message && <div>{d?.message}</div>}
-
-      <div>
-        <h2>Resources</h2>
-        <ul>
-          {resourceList.map((resource) => (
-            <li key={resource.id}>
-              {resource.id}: {resource.email} - {resource.password}
-            </li>
-          ))}
-        </ul>
-      </div>
-
       <Form method="POST">
         <div>
           <label>
